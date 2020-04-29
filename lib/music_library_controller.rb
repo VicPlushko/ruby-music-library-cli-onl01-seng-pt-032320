36f37a1ab805e72d200e1d9af19b05f897d0335e
Victor Plushko <vicplushko33@gmail.com>
2020-04-29 17:41:17 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -29,6 +29,8 @@ class MusicLibraryController
      self.list_genres
    when 'list artist'
      self.list_songs_by_artist
+   when 'list genre'
+     self.list_songs_by_genre
    end
    end
   end
